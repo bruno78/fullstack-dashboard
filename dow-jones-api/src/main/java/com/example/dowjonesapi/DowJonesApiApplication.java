d512b75d6774109fc4c73825febf85f416a7124b
Bruno G. Tavares <devbruno78@gmail.com>
2018-03-18 11:37:07 -0400

    Add dow-jones-api

---
+++ dow-jones-api/src/main/java/com/example/dowjonesapi/DowJonesApiApplication.java
@@ -0,0 +1,22 @@
+package com.example.dowjonesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class DowJonesApiApplication {
+
+	@RequestMapping("/")
+	public String Home() {
+		return "Dow Jones Api is working!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DowJonesApiApplication.class, args);
+	}
+}
